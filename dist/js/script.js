710b9cb39fe53c3f0c21e7a7c7f3734d48b4c044
Cheng_Mo_ <chengmooooo@gmail.com>
2022-05-08 19:12:44 +0200

    complete the project

--- dist/js/script.js
+++ dist/js/script.js
@@ -13,3 +13,18 @@ hamburger.addEventListener('click', function() {
         hamburger.classList.add('open');
     }
 })
+
+// scrolling
+let lastScrollTop = 0;
+
+document.addEventListener('scroll', function () {
+    let st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st > lastScrollTop) {
+        //scroll down
+        hamburger.style.cssText = 'display: none';
+    } else {
+        //scroll up
+        hamburger.style.cssText = 'position: fixed';
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+}, false)
